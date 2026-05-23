658fa853cbd1ca489be66f85a23893048660638b
SamhithaMada <madasamhithareddy@gmail.com>
2026-05-23 09:02:02 +0530

    Completed Task 1

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>ApexPlanet Task 1</title>
+</head>
+<body>
+    <h1>Task 1 Completed</h1>
+    <p>PHP environment configured successfully.</p>
+</body>
+</html>
